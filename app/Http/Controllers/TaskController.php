fe41495079cc3014fae5625815db12ead39caf12
Vicente Chiriguaya <vchiriguayam@hotmail.com>
2023-11-13 11:03:06 -0500

    Cambios

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Task::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "task" => "required"
+        ]);
+
+        $task = Task::create($request->all());
+
+        return response()->json(["message" => "Task has been created!", "task" => $task]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+        $task->save();
+
+        return response()->json(["message" => "Changes has been updated", "task" => $task]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return response()->json(["message" => "Task has been delete."]);
+    }
+}
